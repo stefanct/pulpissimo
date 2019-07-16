62818cd897605429b80478ff6fa6939e5fed0648
bluew <bluewww@users.noreply.github.com>
2019-07-16 17:28:00 +0200

    Revert "Import new bitbang server version"
    This reverts commit 33edf56a0599744d68aaa0f4bce00345d6699e2a.

--- rtl/tb/remote_bitbang/sim_jtag.c
+++ rtl/tb/remote_bitbang/sim_jtag.c
@@ -2,26 +2,22 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include "remote_bitbang.h"
 
 int init = 0;
 
-int jtag_tick(int port, unsigned char *jtag_TCK, unsigned char *jtag_TMS,
-              unsigned char *jtag_TDI, unsigned char *jtag_TRSTn,
-              unsigned char jtag_TDO)
+jtag_tick(unsigned char *jtag_TCK, unsigned char *jtag_TMS,
+          unsigned char *jtag_TDI, unsigned char *jtag_TRSTn,
+          unsigned char jtag_TDO)
 
 {
     if (!init) {
-	if (port < 0 || port > UINT16_MAX)
-	    fprintf(stderr, "Port number of out range: %d\n", port);
-        init = rbs_init(port);
+        // TODO: Pass in real port number
+        init = rbs_init(0);
     }
 
     rbs_tick(jtag_TCK, jtag_TMS, jtag_TDI, jtag_TRSTn, jtag_TDO);
-    if (VERBOSE)
-        fprintf(
-            stderr,
+    fprintf(stderr,
             "Tick with: TCK=%hhd TMS=%hhd TDI=%hhd TRSTn=%hhd --> TDO=%hhd\n",
             *jtag_TCK, *jtag_TMS, *jtag_TDI, *jtag_TRSTn, jtag_TDO);
 
