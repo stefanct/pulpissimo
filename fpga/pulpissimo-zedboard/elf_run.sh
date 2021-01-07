dee8617861e273f8362e2234a71db11b7a0d70b6
Stefan Tauner <stauner@ecs.tuwien.ac.at>
2021-01-07 13:20:24 +0100

    zedboard: generalize elf_run.sh

--- fpga/pulpissimo-zedboard/elf_run.sh
+++ fpga/pulpissimo-zedboard/elf_run.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
 
 SCRIPTDIR=$(dirname $0)
+UART_TTY=${PULP_ZEDBOARD_UART_TTY:=/dev/ttyUSB0}
+UART_BAUDRATE=${PULP_ZEDBOARD_UART_BAUDRATE:=115200}
+TERM_EMU=${PULP_ZEDBOARD_TERM_EMU:=minicom -D}
 
 #Execute gdb and connect to openocd via pipe
-$OPENOCD/bin/openocd -f $SCRIPTDIR/openocd-zedboard-hs2.cfg &
+$OPENOCD/bin/openocd -f $PULP_ZEDBOARD_OPENOCD_CFG &
 $PULP_RISCV_GCC_TOOLCHAIN_CI/bin/riscv32-unknown-elf-gdb -x $SCRIPTDIR/elf_run.gdb $1 &
 sleep 3
-minicom -D /dev/ttyUSB0 -b 115200
+$TERM_EMU $UART_TTY -b $UART_BAUDRATE
 
 
